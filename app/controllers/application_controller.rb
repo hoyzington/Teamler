37c275ac19bb86ddd6889d3c97d44844b9970ff4
github username <github email address>
2020-03-20 20:06:29 -0400

    fixed user show ctrlr/view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:home]
 
   def home
+    if user_signed_in?
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def after_sign_in_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   resources :tasks
   resources :projects
+  resources :users, only: [:show]
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations'}
   root 'application#home'
   get '/about', to: 'static#about'
